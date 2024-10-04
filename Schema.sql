a109c6b3df36819e1bd7e1ee7d96fdae330ad170
Mike Mulchrone <mikemulchrone987@gmail.com>
2024-10-03 21:29:48 -0700

    adding time_series_daily

--- Schema.sql
+++ Schema.sql
@@ -2,6 +2,13 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 DO $$
 BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'time_series_daily') THEN
+        DROP TABLE IF EXISTS public.time_series_daily;
+    END IF;
+END $$;
+
+DO $$
+BEGIN
     IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'tickers') THEN
         DROP TABLE IF EXISTS public.tickers;
     END IF;
@@ -12,4 +19,15 @@ CREATE TABLE public.tickers
     ID UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
     Ticker VARCHAR(50) NOT NULL,
 	IsInPortfolio BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE time_series_daily (
+	ID UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+	Date DATE NOT NULL,
+	Close VARCHAR(250) NOT NULL,
+	High VARCHAR(250) NOT NULL,
+	Low VARCHAR(250) NOT NULL,
+	Opens VARCHAR(250) NOT NULL,
+	Volumne VARCHAR(1000) NOT NULL,
+	Ticker UUID NOT NULL REFERENCES tickers(ID)
 );
